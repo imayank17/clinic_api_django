7f4457eeb2f64f532e0c44044017ce5738b143c6
Mayank <imayank1724@gmail.com>
2026-04-20 16:30:55 +0530

    add upcoming appointments endpoint using DRF @action

--- patients/urls.py
+++ patients/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PatientViewSet, AppointmentViewSet
+from .views import PatientViewSet, AppointmentViewSet, StatsView
 
 router = DefaultRouter()
 router.register(r'patients', PatientViewSet)
 router.register(r'appointments', AppointmentViewSet)
 
 urlpatterns = [
+    path('stats/', StatsView.as_view(), name='stats'),
     path('', include(router.urls)),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, views
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.utils import timezone
 from .models import Patient, Appointment
 from .serializers import PatientSerializer, AppointmentSerializer
 
@@ -22,3 +25,24 @@ class AppointmentViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(patient_id=patient_id)
             
         return queryset
+
+    # GET /api/appointments/upcoming/
+    @action(detail=False, methods=['get'])
+    def upcoming(self, request):
+        upcoming_appointments = Appointment.objects.filter(
+            date__gte=timezone.now(),
+            status="Scheduled"
+        ).order_by('date')
+        
+        serializer = self.get_serializer(upcoming_appointments, many=True)
+        return Response(serializer.data)
+
+class StatsView(views.APIView):
+    def get(self, request):
+        total_patients = Patient.objects.count()
+        total_appointments = Appointment.objects.count()
+        
+        return Response({
+            "total_patients": total_patients,
+            "total_appointments": total_appointments
+        })
