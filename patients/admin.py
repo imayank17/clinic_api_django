daccdef7b438544281070dab793ac17cd13af4e3
Mayank <imayank1724@gmail.com>
2026-04-20 16:02:46 +0530

    regster models in admin and add sample data

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Patient, Appointment
 
-# Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "phone")
+    search_fields = ("name",)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("patient", "doctor_name", "date")
+    list_filter = ("doctor_name",)
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
